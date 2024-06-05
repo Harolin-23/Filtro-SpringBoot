c9069586c84e3ab7acd2990dbe3b5a407271cea5
Harol <132421273+Harolin-23@users.noreply.github.com>
2024-06-05 06:26:52 -0500

    Add files via upload

---
+++ riwiPortalVirtual/src/main/java/com/riwi/riwiPortalVirtual/RiwiPortalVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.riwiPortalVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiPortalVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiPortalVirtualApplication.class, args);
+	}
+
+}
---
+++ riwiPortalVirtual/src/test/java/com/riwi/riwiPortalVirtual/RiwiPortalVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.riwiPortalVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiPortalVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
